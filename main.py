cdfd1fcc397d9c899543b19be3e92b476a82750a
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-30 06:21:45 +0000

    final solution

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 from art import logo, vs
 from game_data import data
+from replit import clear
 
 def format_data(account):
   """Format account data in printable format"""
@@ -21,34 +22,40 @@ def check_answer(guess, a_followers, b_followers):
 print(logo)
 # Keep score for player
 score = 0
-
-# Generate random account from game data
-option_a = random.choice(data)
+should_continue = True
+# Make account B replaced account A on next round
 option_b = random.choice(data)
-if option_a == option_b:
-  option_b = random.choice(data)
-
-print(f"Compare A: {format_data(option_a)}.")
-print(vs)
-print(f"Against B: {format_data(option_b)}.")
-
-# Ask user for input
-guess = input("Who has more followers 'A' or 'B': ").lower()
-
-# Get number of followers 
-a_followers = option_a["follower_count"]
-b_followers = option_b["follower_count"]
-
-is_correct = check_answer(guess, a_followers, b_followers)
-
-# Check if users input answer is correct
-# Give user feedback on answer
-if is_correct:
-  score += 1
-  print(f"You're right. Current score: {score}")
-else:
-  print(f"Sorry you lose. Final score: {score}")
 
 # Make the game repeatable
-# Make account B replaced account A on next round
-# Make screen clear better next round
+while should_continue:
+  # Generate random account from game data
+  option_a = option_b
+  option_b = random.choice(data)
+  while option_a == option_b:
+    option_b = random.choice(data)
+  
+  print(f"Compare A: {format_data(option_a)}.")
+  print(vs)
+  print(f"Against B: {format_data(option_b)}.")
+  
+  # Ask user for input
+  guess = input("Who has more followers 'A' or 'B': ").lower()
+  
+  # Get number of followers 
+  a_followers = option_a["follower_count"]
+  b_followers = option_b["follower_count"]
+  
+  is_correct = check_answer(guess, a_followers, b_followers)
+
+  # Make screen clear better next round
+  clear()
+  print(logo)
+  
+  # Check if users input answer is correct
+  # Give user feedback on answer
+  if is_correct:
+    score += 1
+    print(f"You're right. Current score: {score}")
+  else:
+    print(f"Sorry you lose. Final score: {score}")
+    should_continue = False
